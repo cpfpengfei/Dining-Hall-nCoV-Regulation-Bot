ae298b0beed9f16084ee0de0806f1920694d00d0
JingYing <e0323887@u.nus.edu>
2020-02-10 19:42:18 +0800

    Reminder v1.13

--- reminder.py
+++ reminder.py
@@ -1,12 +1,12 @@
 import datetime
 
+import telegram
+
 def run_morning(update, context):
-    chat_id = update.message.chat_id
-    context.job_queue.run_daily(callback_reminder, datetime.time(0, 00, 00), context=(chat_id, context.bot))
+    context.job_queue.run_daily(callback_reminder, datetime.time(0, 00, 00), context=update.message.chat_id)
 
 def run_night(update, context):
-    chat_id = update.message.chat_id
-    context.job_queue.run_daily(callback_reminder, datetime.time(11, 36, 00), context=(chat_id, context.bot))
+    context.job_queue.run_daily(callback_reminder, datetime.time(11, 45, 00), context=update.message.chat_id)
 
-def callback_reminder(chat_id, bot):
-    bot.send_message(chat_id=chat_id, text='Hello please remember to log your temperature at https://myaces.nus.edu.sg/htd/.')
+def callback_reminder(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id=id, text='Hello please remember to log your temperature at https://myaces.nus.edu.sg/htd/.')
