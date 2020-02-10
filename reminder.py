853762da85ab6cc4d4cee3958bd7b98caf82154a
JingYing <e0323887@u.nus.edu>
2020-02-10 19:33:59 +0800

    Reminder v1.12

--- reminder.py
+++ reminder.py
@@ -2,11 +2,11 @@ import datetime
 
 def run_morning(update, context):
     chat_id = update.message.chat_id
-    context.job_queue.run_daily(callback_reminder, datetime.time(0, 00, 00), context=(chat_id, update.bot))
+    context.job_queue.run_daily(callback_reminder, datetime.time(0, 00, 00), context=(chat_id, context.bot))
 
 def run_night(update, context):
     chat_id = update.message.chat_id
-    context.job_queue.run_daily(callback_reminder, datetime.time(11, 33, 00), context=(chat_id, update.bot))
+    context.job_queue.run_daily(callback_reminder, datetime.time(11, 36, 00), context=(chat_id, context.bot))
 
 def callback_reminder(chat_id, bot):
     bot.send_message(chat_id=chat_id, text='Hello please remember to log your temperature at https://myaces.nus.edu.sg/htd/.')
